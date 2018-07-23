86c65d8fd69e534d7b874dad033d365ea4709e71
kongo2002 <kongo2002@googlemail.com>
2018-07-23 22:14:34 +0200

    first layout sketch using skeleton

--- client/src/View.elm
+++ client/src/View.elm
@@ -1,13 +1,40 @@
 module View exposing ( view )
 
 import Html exposing (..)
+import Html.Attributes exposing (..)
 
 import Types exposing (..)
 
 
 view : Model -> Html Msg
 view model =
-  p [] [ text "hi, from elm!" ]
+  div [ class "container" ]
+  [ navigation model
+  , body model
+  ]
+
+
+navigation : Model -> Html Msg
+navigation model =
+  let link ref name =
+        li [] [ a [ href ref ] [ text name ] ]
+  in div [ class "row" ]
+     [ div [ id "brand", class "four columns" ]
+       [ h1 [] [ text "ogonek" ] ]
+     , div [ id "nav", class "eight columns" ]
+       [ ul []
+         [ link "#login" "login"
+         , link "#help" "help"
+         ]
+       ]
+     ]
+
+
+body : Model -> Html Msg
+body model =
+  div [ class "row" ]
+  [ p [] [ text "welcome to ogonek!" ]
+  ]
 
 
 -- vim: et sw=2 sts=2
--- client/src/index.js
+++ client/src/index.js
@@ -2,6 +2,10 @@
 
 require('./index.html');
 
+import './css/normalize.css';
+import './css/skeleton.css';
+import './css/custom.css';
+
 /* build websocket target host based on current location */
 var wsHost = ((window.location.protocol === "https:") ? "wss://" : "ws://") + window.location.host + "/.ws"
 var elm = require('./Main.elm');
