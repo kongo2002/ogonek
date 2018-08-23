43eff478f922740d243f95acd057143aa66e4038
kongo2002 <kongo2002@googlemail.com>
2018-08-23 21:53:41 +0200

    client: improve deltaToString

--- client/src/Utils.elm
+++ client/src/Utils.elm
@@ -21,15 +21,17 @@ import Time.DateTime
 
 deltaToString : Time.DateTime.DateTimeDelta -> String
 deltaToString delta =
-  let fmt value unit = toString value ++ " " ++ unit
+  let fmt value oneUnit multiples =
+      let unit = if value == 1 then oneUnit else multiples
+      in  toString value ++ " " ++ unit
   in  if delta.days > 0 then
-        fmt delta.days "days"
+        fmt delta.days "day" "days"
       else if delta.hours > 0 then
-        fmt delta.hours "hours"
+        fmt delta.hours "hour" "hours"
       else if delta.minutes > 0 then
-        fmt delta.minutes "minutes"
+        fmt delta.minutes "minute" "minutes"
       else
-        fmt delta.seconds "seconds"
+        fmt delta.seconds "second" "seconds"
 
 
 planetToString : PlanetType -> String
